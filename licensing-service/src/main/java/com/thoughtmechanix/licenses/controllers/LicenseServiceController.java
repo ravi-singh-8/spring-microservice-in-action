db60373369685e3d4265cd71d2b0268f91bf0454
Ravi <ravi.kmr.singh8@hotmail.com>
2023-06-02 23:52:18 +0530

    Dockerfile and Docker compose

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -3,6 +3,9 @@ package com.thoughtmechanix.licenses.controllers;
 import com.thoughtmechanix.licenses.config.LicensingServiceConfig;
 import com.thoughtmechanix.licenses.models.License;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class LicenseServiceController {
 
     @Autowired
-    LicensingServiceConfig config;
+    private Environment environment;
+
+    @Autowired
+    private LicensingServiceConfig config;
 
     @GetMapping(value="/{licenseId}")
-    public License getLicenses(@PathVariable("orgId")String orgId,
-                               @PathVariable("licenseId") String licenseId) {
-        return new License()
+    public ResponseEntity<License> getLicenses(@PathVariable("orgId")String orgId,
+                                              @PathVariable("licenseId") String licenseId) {
+        HttpHeaders headers = new HttpHeaders();
+        String port = environment.getProperty("local.server.port");
+        headers.add("server.port",  port);
+
+        License license = new License()
                 .withId(licenseId)
                 .withProductName("Telco")
                 .withLicenseType("Seat")
                 .withOrganizationId(orgId);
+        return ResponseEntity.ok().headers(headers).body(license);
     }
 
     @RequestMapping(value = "config")
